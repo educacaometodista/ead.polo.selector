8109084763ba65c378fb1b248211360c9e5e23fe
LeuAlmeida <leo@webid.net.br>
2019-10-16 14:50:01 -0300

    Insert data infos

---
+++ data.js
@@ -0,0 +1,424 @@
+const data = {
+  al: [
+    {
+      title: "Maceió",
+      url: "http://portal.metodista.br/ead/graduacao-a-distancia/polos/maceio"
+    }
+  ],
+  ba: [
+    {
+      title: "Vitória da Conquista",
+      url:
+        "http://portal.metodista.br/ead/graduacao-a-distancia/polos/vitoria-da-conquista"
+    }
+  ],
+  ce: [
+    {
+      title: "Sobral",
+      url: "http://portal.metodista.br/ead/graduacao-a-distancia/polos/sobral/"
+    }
+  ],
+  df: [
+    {
+      title: "Brasília",
+      url: "http://portal.metodista.br/ead/graduacao-a-distancia/polos/brasilia"
+    },
+    {
+      title: "Ceilândia",
+      url:
+        "http://portal.metodista.br/ead/graduacao-a-distancia/polos/ceilandia"
+    }
+  ],
+  es: [
+    {
+      title: "Cariacica (unidade de apoio do polo Vitória)",
+      url:
+        "http://portal.metodista.br/ead/graduacao-a-distancia/polos/cariacica"
+    },
+    {
+      title: "Guarapari",
+      url:
+        "http://portal.metodista.br/ead/graduacao-a-distancia/polos/guarapari"
+    },
+    {
+      title: "Serra (unidade de apoio do polo Vitória)",
+      url: "http://portal.metodista.br/ead/graduacao-a-distancia/polos/serra"
+    },
+    {
+      title: "Vila Velha (unidade de apoio do polo Vitória)",
+      url:
+        "http://portal.metodista.br/ead/graduacao-a-distancia/polos/vila-velha"
+    },
+    {
+      title: "Vitória",
+      url: "http://portal.metodista.br/ead/graduacao-a-distancia/polos/vitoria"
+    }
+  ],
+  go: [
+    {
+      title: "Valparaíso de Goiás",
+      url:
+        "http://portal.metodista.br/ead/graduacao-a-distancia/polos/valparaiso-de-goias"
+    }
+  ],
+  ma: [
+    {
+      title: "Imperatriz",
+      url:
+        "http://portal.metodista.br/ead/graduacao-a-distancia/polos/imperatriz"
+    }
+  ],
+  mt: [
+    {
+      title: "Guarantã do Norte",
+      url:
+        "http://portal.metodista.br/ead/graduacao-a-distancia/polos/guaranta-do-norte"
+    },
+    {
+      title: "Várzea Grande",
+      url:
+        "http://portal.metodista.br/ead/graduacao-a-distancia/polos/varzea-grande"
+    }
+  ],
+  mg: [
+    {
+      title: "Arantina",
+      url: "http://portal.metodista.br/ead/graduacao-a-distancia/polos/arantina"
+    },
+    {
+      title: "Belo Horizonte - Praça da Liberdade",
+      url:
+        "http://portal.metodista.br/ead/graduacao-a-distancia/polos/belo-horizonte-praca-da-liberdade"
+    },
+    {
+      title: "Juiz de Fora (Granbery)",
+      url:
+        "http://portal.metodista.br/ead/graduacao-a-distancia/polos/juiz-de-fora"
+    },
+    {
+      title: "Vila da Serra (Nova Lima – MG)",
+      url:
+        "http://portal.metodista.br/ead/graduacao-a-distancia/polos/nova-lima"
+    },
+    {
+      title: "Uberlândia",
+      url:
+        "http://portal.metodista.br/ead/graduacao-a-distancia/polos/minas-gerais/uberlandia"
+    }
+  ],
+  pa: [
+    {
+      title: "Altamira",
+      url: "http://portal.metodista.br/ead/graduacao-a-distancia/polos/altamira"
+    }
+  ],
+  pb: [
+    {
+      title: "Campina Grande",
+      url:
+        "http://portal.metodista.br/ead/graduacao-a-distancia/polos/campina-grande"
+    },
+    {
+      title: "Pitimbu (unidade de apoio do polo Recife)",
+      url: "http://portal.metodista.br/ead/graduacao-a-distancia/polos/pitimbu"
+    }
+  ],
+  pr: [
+    {
+      title: "Arapongas",
+      url:
+        "http://portal.metodista.br/ead/graduacao-a-distancia/polos/arapongas"
+    },
+    {
+      title: "Curitiba",
+      url:
+        "http://portal.metodista.br/ead/graduacao-a-distancia/polos/curitiba/"
+    }
+  ],
+  pe: [
+    {
+      title: "Arco Verde (unidade de apoio do polo Recife)",
+      url:
+        "http://portal.metodista.br/ead/graduacao-a-distancia/polos/arco-verde"
+    },
+    {
+      title: "Caruaru (unidade de apoio do polo Recife)",
+      url: "http://portal.metodista.br/ead/graduacao-a-distancia/polos/caruaru"
+    },
+    {
+      title: "Cabo de Santo Agostinho (unidade de apoio do polo Recife)",
+      url:
+        "http://portal.metodista.br/ead/graduacao-a-distancia/polos/cabo-de-santo-agostinho"
+    },
+    {
+      title: "Olinda (unidade de apoio do polo Recife)",
+      url: "http://portal.metodista.br/ead/graduacao-a-distancia/polos/olinda"
+    },
+    {
+      title: "Ouricuri (unidade de apoio do polo Recife)",
+      url: "http://portal.metodista.br/ead/graduacao-a-distancia/polos/ouricuri"
+    },
+    {
+      title: "Palmares (unidade de apoio do polo Recife)",
+      url: "http://portal.metodista.br/ead/graduacao-a-distancia/polos/palmares"
+    },
+    {
+      title: "Petrolina (unidade de apoio do polo Recife)",
+      url:
+        "http://portal.metodista.br/ead/graduacao-a-distancia/polos/petrolina"
+    },
+    {
+      title: "Recife",
+      url: "http://portal.metodista.br/ead/graduacao-a-distancia/polos/recife"
+    },
+    {
+      title: "Recife, Bairro Boa viagem (unidade de apoio do polo Recife)",
+      url:
+        "http://portal.metodista.br/ead/graduacao-a-distancia/polos/boa-viagem"
+    },
+    {
+      title: "Recife, Bairro Casa Forte (unidade de apoio do polo Recife)",
+      url:
+        "http://portal.metodista.br/ead/graduacao-a-distancia/polos/casa-forte"
+    },
+    {
+      title: "Recife, Bairro Recife Antigo (unidade de apoio do polo Recife)",
+      url:
+        "http://portal.metodista.br/ead/graduacao-a-distancia/polos/antigo-recife"
+    },
+    {
+      title: "Recife, Bairro Várzea (unidade de apoio do polo Recife)",
+      url: "http://portal.metodista.br/ead/graduacao-a-distancia/polos/varzea"
+    }
+  ],
+  rj: [
+    {
+      title: "Engenheiro Paulo de Frontin",
+      url:
+        "http://portal.metodista.br/ead/graduacao-a-distancia/polos/engenheiro-paulo-de-frontim"
+    },
+    {
+      title: "Macaé",
+      url: "http://portal.metodista.br/ead/graduacao-a-distancia/polos/macae"
+    },
+    {
+      title: "Nova Iguaçu",
+      url:
+        "http://portal.metodista.br/ead/graduacao-a-distancia/polos/nova-iguacu"
+    },
+    {
+      title: "Petrópolis",
+      url:
+        "http://portal.metodista.br/ead/graduacao-a-distancia/polos/petropolis"
+    },
+    {
+      title: "Rio de Janeiro (Centro)",
+      url:
+        "http://portal.metodista.br/ead/graduacao-a-distancia/polos/rio-de-janeiro"
+    },
+    {
+      title: "Rio de Janeiro - Realengo ",
+      url:
+        "http://portal.metodista.br/ead/graduacao-a-distancia/polos/rio-de-janeiro-realengo"
+    },
+    {
+      title: "Volta Redonda",
+      url:
+        "http://portal.metodista.br/ead/graduacao-a-distancia/polos/volta-redonda"
+    }
+  ],
+  rs: [
+    {
+      title: "Dom Pedrito",
+      url: "http://portal.metodista.br/ead/graduacao-a-distancia/polos/dom-pedrito",
+    },
+    {
+      title: "Passo Fundo",
+      url: "http://portal.metodista.br/ead/graduacao-a-distancia/polos/passo-fundo/",
+    },
+    {
+      title: "Porto Alegre",
+      url: "http://portal.metodista.br/ead/graduacao-a-distancia/polos/rio-grande-do-sul/porto-alegre",
+    },
+    {
+      title: "Santa Maria",
+      url: "http://portal.metodista.br/ead/graduacao-a-distancia/polos/santa-maria/",
+    },
+    {
+      title: "Uruguaiana ",
+      url: "http://portal.metodista.br/ead/graduacao-a-distancia/polos/uruguaiana",
+    },
+  ],
+  ro: [
+    {
+      title: 'Porto Velho',
+      url: 'http://portal.metodista.br/ead/graduacao-a-distancia/polos/porto-velho',
+    },
+  ],
+  sc: [
+    {
+      title: 'Blumenau',
+      url: 'http://portal.metodista.br/ead/graduacao-a-distancia/polos/blumenau',
+    },
+  ],
+  sp: [
+    {
+      title: 'Bauru',
+      url: 'http://portal.metodista.br/ead/graduacao-a-distancia/polos/bauru',
+    },
+    {
+      title: 'Bertioga',
+      url: 'http://portal.metodista.br/ead/graduacao-a-distancia/polos/bertioga',
+    },
+    {
+      title: 'Birigui',
+      url: 'http://portal.metodista.br/ead/graduacao-a-distancia/polos/birigui',
+    },
+    {
+      title: 'Bragança Paulista',
+      url: 'http://portal.metodista.br/ead/graduacao-a-distancia/polos/braganca-paulista',
+    },
+    {
+      title: 'Campinas',
+      url: 'http://portal.metodista.br/ead/graduacao-a-distancia/polos/campinas',
+    },
+    {
+      title: 'Diadema',
+      url: 'http://portal.metodista.br/ead/graduacao-a-distancia/polos/diadema/',
+    },
+    {
+      title: 'Embu-Guaçu',
+      url: 'http://portal.metodista.br/ead/graduacao-a-distancia/polos/embu-guacu',
+    },
+    {
+      title: 'Franca',
+      url: 'http://portal.metodista.br/ead/graduacao-a-distancia/polos/franca',
+    },
+    {
+      title: 'Guaratinguetá',
+      url: 'http://portal.metodista.br/ead/graduacao-a-distancia/polos/guaratingueta',
+    },
+    {
+      title: 'Itanhaém',
+      url: 'http://portal.metodista.br/ead/graduacao-a-distancia/polos/itanhaem',
+    },
+    {
+      title: 'Itapeva',
+      url: 'http://portal.metodista.br/ead/graduacao-a-distancia/polos/itapeva',
+    },
+    {
+      title: 'Itaquaquecetuba (unidade de apoio do polo Guaianases)',
+      url: 'http://portal.metodista.br/ead/graduacao-a-distancia/polos/itaquaquecetuba',
+    },
+    {
+      title: 'Jundiaí',
+      url: 'http://portal.metodista.br/ead/graduacao-a-distancia/polos/jundiai',
+    },
+    {
+      title: 'Limeira',
+      url: 'http://portal.metodista.br/ead/graduacao-a-distancia/polos/sao-paulo/limeira',
+    },
+    {
+      title: 'Lins',
+      url: 'http://portal.metodista.br/ead/graduacao-a-distancia/polos/lins',
+    },
+    {
+      title: 'Mauá',
+      url: 'http://portal.metodista.br/ead/graduacao-a-distancia/polos/maua',
+    },
+    {
+      title: 'Osasco',
+      url: 'http://portal.metodista.br/ead/graduacao-a-distancia/polos/osasco',
+    },
+    {
+      title: 'Piracicaba - ACIPI',
+      url: 'http://portal.metodista.br/ead/graduacao-a-distancia/polos/piracicaba-acipi',
+    },
+    {
+      title: 'Piracicaba - Centro',
+      url: 'http://portal.metodista.br/ead/graduacao-a-distancia/polos/piracicaba',
+    },
+    {
+      title: 'Piracicaba - Taquaral',
+      url: 'http://portal.metodista.br/ead/graduacao-a-distancia/polos/piracicaba-taquaral',
+    },
+    {
+      title: 'Praia Grande',
+      url: 'http://portal.metodista.br/ead/graduacao-a-distancia/polos/praia-grande',
+    },
+    {
+      title: 'Presidente Prudente',
+      url: 'http://portal.metodista.br/ead/graduacao-a-distancia/polos/ribeirao-preto',
+    },
+    {
+      title: 'Ribeirão Preto',
+      url: 'http://portal.metodista.br/ead/graduacao-a-distancia/polos/ribeirao-preto',
+    },
+    {
+      title: 'São Bernardo do Campo - Planalto',
+      url: 'http://portal.metodista.br/ead/graduacao-a-distancia/polos/planalto',
+    },
+    {
+      title: 'São Bernardo do Campo - Rudge Ramos',
+      url: 'http://portal.metodista.br/ead/graduacao-a-distancia/polos/rudge-ramos',
+    },
+    {
+      title: 'São Bernardo do Campo - Vergueiro',
+      url: 'http://portal.metodista.br/ead/graduacao-a-distancia/polos/vergueiro',
+    },
+    {
+      title: 'Santa Bárbara d‘Oeste (Unimep)',
+      url: 'http://portal.metodista.br/ead/graduacao-a-distancia/polos/santa-barbara-d-oeste',
+    },
+    {
+      title: 'Santos',
+      url: 'http://portal.metodista.br/ead/graduacao-a-distancia/polos/santos',
+    },
+    {
+      title: 'São José do Rio Preto',
+      url: 'http://portal.metodista.br/ead/graduacao-a-distancia/polos/sao-jose-do-rio-preto',
+    },
+    {
+      title: 'São José dos Campos',
+      url: 'http://portal.metodista.br/ead/graduacao-a-distancia/polos/sao-jose-dos-campos',
+    },
+    {
+      title: 'São Paulo - Guaianases',
+      url: 'http://portal.metodista.br/ead/graduacao-a-distancia/polos/guaianazes',
+    },
+    {
+      title: 'São Paulo - Jabaquara',
+      url: 'http://portal.metodista.br/ead/graduacao-a-distancia/polos/sao-paulo-jabaquara',
+    },
+    {
+      title: 'São Paulo - Lapa',
+      url: 'http://portal.metodista.br/ead/graduacao-a-distancia/polos/sao-paulo-lapa',
+    },
+    {
+      title: 'São Paulo - Mooca',
+      url: 'http://portal.metodista.br/ead/graduacao-a-distancia/polos/sao-paulo-mooca',
+    },
+    {
+      title: 'São Paulo - Paulista (Consolação)',
+      url: 'http://portal.metodista.br/ead/graduacao-a-distancia/polos/paulista',
+    },
+    {
+      title: 'São Paulo - Perus',
+      url: 'http://portal.metodista.br/ead/graduacao-a-distancia/polos/perus',
+    },
+    {
+      title: 'São Paulo - Santo Amaro',
+      url: 'http://portal.metodista.br/ead/graduacao-a-distancia/polos/santo-amaro/',
+    },
+    {
+      title: 'São Paulo - Tatuapé',
+      url: 'http://portal.metodista.br/ead/graduacao-a-distancia/polos/tatuape',
+    },
+    {
+      title: 'Sorocaba',
+      url: 'http://portal.metodista.br/ead/graduacao-a-distancia/polos/sorocaba',
+    },
+  ],
+};
+
+export default data;
